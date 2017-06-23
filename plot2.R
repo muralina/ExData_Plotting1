935a1f5ad3c0bdc54022ee39ca2c3a46cb4c084e
muralina <nithyamurali16@gmail.com>
2017-06-23 15:06:08 -0400

    plot2.R

---
+++ plot2.R
@@ -0,0 +1,12 @@
+##file name stored in variable
+consfile <- "household_power_consumption.txt"
+
+##read data and subset the data and plot
+data <- read.table(consfile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(subData$Global_active_power)
+dt <- strptime(paste(subData$Date, subData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+gPower <- as.numeric(subData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(dt, gPower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
